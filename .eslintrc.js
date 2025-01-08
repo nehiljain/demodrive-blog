1bb2e3315a2f6957c5dd3428a215e871d3d84fad
Nehil Jain <2761999+nehiljain@users.noreply.github.com>
2025-01-08 00:13:14 -0500

    Testing husky precommit hook

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react", "unused-imports"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "next/core-web-vitals",
+    "prettier",
+  ],
+  rules: {
+    // Customize rules as needed
+    "unused-imports/no-unused-imports": "error",
+  },
+}
